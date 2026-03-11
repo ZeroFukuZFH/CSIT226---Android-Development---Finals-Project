8b0b92703f1b1139da6cc381506e82c87794a2e5
ZeroFuku <12mnsin@gmail.com>
2026-03-11 23:53:09 +0800

    Too lazy to commit each, just some file commits

--- app/src/main/java/com/example/myapplication/FooterLayout.kt
+++ app/src/main/java/com/example/myapplication/FooterLayout.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
@@ -16,22 +17,26 @@ class FooterLayout : Activity() {
         val linearLayoutProfileActivity = findViewById<LinearLayout>(R.id.linearLayoutProfileActivity)
 
         linearLayoutDashboardActivity.setOnClickListener {
-
+            val dashboardActivity = Intent(this,DashboardActivity::class.java)
+            startActivity(dashboardActivity)
             Log.e("HOME_BUTTON","Home Button has been Pressed")
         }
 
         linearLayoutCafesActivity.setOnClickListener {
-
+            val dashboardActivity = Intent(this,DashboardActivity::class.java)
+            startActivity(dashboardActivity)
             Log.e("CAFES_BUTTON","Cafes Button has been Pressed")
         }
 
         linearLayoutSocialActivity.setOnClickListener {
-
+            val dashboardActivity = Intent(this,DashboardActivity::class.java)
+            startActivity(dashboardActivity)
             Log.e("SOCIAL_BUTTON","Social Button has been Pressed")
         }
 
         linearLayoutProfileActivity.setOnClickListener {
-
+            val dashboardActivity = Intent(this,DashboardActivity::class.java)
+            startActivity(dashboardActivity)
             Log.e("PROFILE_BUTTON","Profile Button has been Pressed")
         }
     }
---
+++ app/src/main/java/com/example/myapplication/SocialActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class SocialActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social)
+    }
+}
